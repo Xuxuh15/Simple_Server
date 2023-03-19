4e1d29392207f6e4155528e5e969a4213a750354
Xuxuh <jk.hilton@hotmail.com>
2023-03-19 13:22:21 +0100

    First commit

--- js/index.js
+++ js/index.js
@@ -1,20 +1,12 @@
 const http = require('http');  //http server object
-const date = new Date();     //a new Date object
-const hour = date.getHours();  //gets the current hour 
+const fs = require('fs'); 
+const greetingPage = fs.readFileSync('../html/index.html');  
 
  
 
 //creates http server. Depending on the hour of day, returns a different greeting 
 const server = http.createServer((req,res)=>{
     console.log(req.url); 
-    if(hour < 12){
-        res.end('Good Morning World')
-    }
-    else if(hour >= 12 && hour < 18){
-        res.end('Good Afternoon World');
-    }
-    else{
-        res.end('Good Evening World'); 
-    }
-})
+    res.end(greetingPage);
+}); 
 server.listen(3000); 
